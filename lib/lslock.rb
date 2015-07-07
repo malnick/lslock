9c48327191979c13e5b4313cda0cd7e84e75cd93
malnick <malnick@gmail..com>
2015-07-06 18:19:00 -0700

    I think this is it?"

--- lib/lslock.rb
+++ lib/lslock.rb
@@ -8,12 +8,18 @@ class Lslock
   end
   
   def self.list(path)
+    # I'm not sure if I need to have this class share the lock info yet...
+    @locks = Hash.new
     LOG.info "Known processes in #{path}:"
+    # For all entries in path
     Dir.entries(path).each do |l|
-      unless l == '.' || l == '..' 
-        LOG.info "Parsing #{l}"
+      # Don't care about dot dirs
+      unless l == '.' || l == '..'
+        # Read the file and print it to stdout 
         File.open("#{path}/#{l}", 'r') do |f|
           LOG.info "Process #{path}/#{l}: #{f.read}"
+          # populate the locks hash, ensure it's an instance var so I can expose it later as an API
+          @locks["#{path}/#{l}"] = f.read
         end
       end
     end
--- tests/lslock-test.rb
+++ tests/lslock-test.rb
@@ -1,13 +1,18 @@
 class LslockTest
-  def self.fork(procs)
+  def self.fork(procs, time)
     procs.times do |f|
       Process.fork do  
-        File.open("/tmp/test/#{Process.pid}.lock", 'wb') do |p|
-          p.write Process.pid 
-        end
+        new_proc(time)
       end
     end
   end
+
+  def self.new_proc(time)
+    File.open("/tmp/test/#{Process.pid}.lock", 'wb') do |p|
+      p.write Process.pid 
+    end
+    sleep time
+  end
 end
 
-LslockTest.fork(3)
+LslockTest.fork(3, 10000)
